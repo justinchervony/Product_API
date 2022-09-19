f560388f13b7fccdf5b5fa927ce3cf173efa29a3
unknown <justinchervony@gmail.com>
2022-09-19 14:01:56 -0400

    Implemented admin page, get requests. Added two products.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def products_list(request):
-    return Response('ok')
-    pass
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
