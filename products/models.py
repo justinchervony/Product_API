792f4f1387d63e53514ae93f3dc518bea0464bec
unknown <justinchervony@gmail.com>
2022-09-19 21:51:53 -0400

    Added 'picture' attribute to products. Exported postman requests to project folder.

--- products/models.py
+++ products/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+    picture = models.CharField(max_length=255, default = 'no picture')
     price = models.DecimalField(max_digits=9, decimal_places=2)
     inventory_quantity = models.IntegerField()
